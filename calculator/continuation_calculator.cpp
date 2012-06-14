69cce024242cfd60b0338c27b8272d721dad7883
Johnicholas Hines <johnicholas.hines@gmail.com>
2012-06-14 18:20:09 -0400

    C++ implementation of tail-calling continuation calculator

--- calculator/continuation_calculator.cpp
+++ calculator/continuation_calculator.cpp
@@ -2,45 +2,16 @@
 // useful as an example of an interpreter,
 // based extremely loosely on Andrej Bauer's 'calc' in his PL Zoo.
 //
-// The language has a grammar:
-//   an expression can be Numeral containing a primitive int
-//   an expression can be Plus containing two expressions
-//   an expression can be Minus containing two expressions
-//   an expression can be Times containing two expressions
-//   an expression can be Divide containing two expressions
-//   an expression can be Negate contianing one expression
-//
-// There is another grammar for the cont:
-//   a cont can be Halt
-//   a cont can be PlusK1 of an exp and a cont
-//   a cont can be PlusK2 of an int and a cont
-//   a cont can be MinusK1 of an exp and a cont
-//   a cont can be MinusK2 of an int and a cont
-//   a cont can be TimesK1 of an exp and a cont
-//   a cont can be TimesK2 of an int and a cont
-//   a cont can be DivideK1 of an exp and a cont
-//   a cont can be DivideK2 of an int and a cont
-//
-// This is "the same" interpreter, but it is converted into
-// continuation passing style, which means that every recursive call
-// is a tail call, and we manage an explicit continuation "stack",
-// pushing a 'todo' reminder onto it whenever we would have done
-// some sort of sequenced computation.
-//
-// Notice that, because every call is a tail call,
-// we can manually do the tail-call optimization,
-// replacing recursion with mutation.
-// Now the body of the 'calc' function looks, if you squint,
-// like a Soar decision cycle (the while(true) loop),
-// with a pattern matching network (the ifelse tree),
-// and operator applications (the assignments).
-// It's recognizably a problem space with fifteen simple operators.
-// Unfortunately, it is very flat - it no longer has the nice
-// ragged-left-edge that we could chunk over.
-//
-// Aside: Since everything in this world (derived from the original
-// term-rewriting implementation) is acyclic, we can stop leaking
-// memory just by using reference counting.
+// I understand this is terrible C++ code,
+// I understand it will leak memory continuously
+// (unless we use something like Bohm conservative gc),
+// I understand it is not good OO style.
+// The point is to gesture at what this would look like in C,
+// without actually writing enums and unions.
+// 
+// This continuation_calculator has manual tail-call
+// optimization applied, which means (since it's 
+// in continuation-passing style), that it's very flat.
 
 #include <cassert>
 #include <cstdio>
@@ -99,187 +70,205 @@ public:
   virtual ~Cont() {}
 };
 
+class Plusk1 : public Cont {
+public:
+  Plusk1(Exp* exp, Cont* k) : exp(exp), k(k) {}
+  Exp* exp;
+  Cont* k;
+};
+
+class Plusk2 : public Cont {
+public:
+  Plusk2(int v, Cont* k) : v(v), k(k) {}
+  int v;
+  Cont* k;
+};
+
+class Minusk1 : public Cont {
+public:
+  Minusk1(Exp* exp, Cont* k) : exp(exp), k(k) {}
+  Exp* exp;
+  Cont* k;
+};
+
+class Minusk2 : public Cont {
+public:
+  Minusk2(int v, Cont* k) : v(v), k(k) {}
+  int v;
+  Cont* k;
+};
+
+class Timesk1 : public Cont {
+public:
+  Timesk1(Exp* exp, Cont* k) : exp(exp), k(k) {}
+  Exp* exp;
+  Cont* k;
+};
+
+class Timesk2 : public Cont {
+public:
+  Timesk2(int v, Cont* k) : v(v), k(k) {}
+  int v;
+  Cont* k;
+};
+
+class Dividek1 : public Cont {
+public:
+  Dividek1(Exp* exp, Cont* k) : exp(exp), k(k) {}
+  Exp* exp;
+  Cont* k;
+};
+
+class Dividek2 : public Cont {
+public:
+  Dividek2(int v, Cont* k) : v(v), k(k) {}
+  int v;
+  Cont* k;
+};
+
 class Halt : public Cont {
 public:
-Halt() {}
+  Halt() {}
 };
 
-class PlusK1 : public Cont {
-public:
-  PlusK1(Exp* exp, Cont* cont) : exp(exp), cont(cont) {}
-  Exp* exp;
-  Cont* cont;
-};
-
-class PlusK2 : public Cont {
-public:
-  PlusK2(int v, Cont* cont) : v(v), cont(cont) {}
-  int v;
-  Cont* cont;
-};
-
-class MinusK1 : public Cont {
-public:
-  MinusK1(Exp* exp, Cont* cont) : exp(exp), cont(cont) {}
-  Exp* exp;
-  Cont* cont;
-};
-
-class MinusK2 : public Cont {
-public:
-  MinusK2(int v, Cont* cont) : v(v), cont(cont) {}
-  int v;
-  Cont* cont;
-};
-
-class TimesK1 : public Cont {
-public:
-  TimesK1(Exp* exp, Cont* cont) : exp(exp), cont(cont) {}
-  Exp* exp;
-  Cont* cont;
-};
-
-class TimesK2 : public Cont {
-public:
-  TimesK2(int v, Cont* cont) : v(v), cont(cont) {}
-  int v;
-  Cont* cont;
-};
-
-class DivideK1 : public Cont {
-public:
-  DivideK1(Exp* exp, Cont* cont) : exp(exp), cont(cont) {}
-  Exp* exp;
-  Cont* cont;
-};
-
-class DivideK2 : public Cont {
-public:
-  DivideK2(int v, Cont* cont) : v(v), cont(cont) {}
-  int v;
-  Cont* cont;
-};
 
 void print_indented(int indent, const char* to_print) {
   for (int i= 0; i < indent; ++i) {
-    printf(" ");
+    printf("   ");
   }
-  printf("%s\n", to_print);
+  printf("(%s)\n", to_print);
 }
 
-int run(Exp* initial_exp, int indent) {
-  print_indented(indent, "run");
-  indent += 1;
+int calc(Exp* initial_exp, int indent) {
   Exp* exp = initial_exp;
-  Cont* cont = new Halt();
-  int* v = NULL;
+  Cont* k = new Halt();
+  bool v_is_here = false;
+  int v;
+  print_indented(indent, "calc");
 
   while (true) {
-    if (dynamic_cast<Numeral*>(exp) and cont) {
-      print_indented(indent, "run*numeral");
-      Numeral* numeral = dynamic_cast<Numeral*>(exp);
-
-      v = new int(numeral->n);
+    if (dynamic_cast<Numeral*>(exp) and k) {
+      print_indented(indent+1, "numeral");
+      v_is_here = true;
+      v = dynamic_cast<Numeral*>(exp)->n;
       exp = NULL;
-    } else if(dynamic_cast<Plus*>(exp) and cont) {    
-      print_indented(indent, "run*plus");
-      Plus* plus = dynamic_cast<Plus*>(exp);
+      continue;
+    }
 
-      cont = new PlusK1(plus->right, cont);
-      exp = plus->left;
-    } else if (dynamic_cast<Minus*>(exp) and cont) {
-      print_indented(indent, "run*minus");
-      Minus* minus = dynamic_cast<Minus*>(exp);
+    if (dynamic_cast<Plus*>(exp) and k) {
+      print_indented(indent+1, "plus");
+      k = new Plusk1(dynamic_cast<Plus*>(exp)->right, k);
+      exp = dynamic_cast<Plus*>(exp)->left;
+      continue;
+    }
 
-      cont = new MinusK1(minus->right, cont);
-      exp = minus->left;
-    } else if (dynamic_cast<Times*>(exp) and cont) {
-      print_indented(indent, "run*times");
-      Times* times = dynamic_cast<Times*>(exp);
+    if (dynamic_cast<Minus*>(exp) and k) {
+      print_indented(indent+1, "minus");
+      k = new Minusk1(dynamic_cast<Minus*>(exp)->right, k);
+      exp = dynamic_cast<Minus*>(exp)->left;
+      continue;
+    }
 
-      cont = new TimesK1(times->right, cont);
-      exp = times->left;
-    } else if (dynamic_cast<Divide*>(exp) and cont) {
-      print_indented(indent, "run*divide");
-      Divide* divide = dynamic_cast<Divide*>(exp);
+    if (dynamic_cast<Times*>(exp) and k) {
+      print_indented(indent+1, "times");
+      k = new Timesk1(dynamic_cast<Times*>(exp)->right, k);
+      exp = dynamic_cast<Times*>(exp)->left;
+      continue;
+    }
 
-      cont = new DivideK1(divide->right, cont);
-      exp = divide->left;
-    } else if (dynamic_cast<Negate*>(exp) and cont) {
-      print_indented(indent, "run*negate");
-      Negate* negate = dynamic_cast<Negate*>(exp);
+    if (dynamic_cast<Divide*>(exp) and k) {
+      print_indented(indent+1, "divide");
+      k = new Dividek1(dynamic_cast<Divide*>(exp)->right, k);
+      exp = dynamic_cast<Divide*>(exp)->left;
+      continue;
+    }
 
-      exp = new Minus(new Numeral(0), negate->exp);
-    } else if (dynamic_cast<PlusK1*>(cont) and v) {
-      print_indented(indent, "run*plusk1");
-      PlusK1* plusk1 = dynamic_cast<PlusK1*>(cont);
+    if (dynamic_cast<Negate*>(exp) and k) {
+      print_indented(indent+1, "negate");
+      exp = new Minus(new Numeral(0), dynamic_cast<Negate*>(exp)->exp);
+      continue;
+    }
 
-      exp = plusk1->exp;
-      cont = new PlusK2(*v, plusk1->cont);
-      v = NULL;
-    } else if (dynamic_cast<PlusK2*>(cont) and v) {
-      print_indented(indent, "run*plusk2");
-      PlusK2* plusk2 = dynamic_cast<PlusK2*>(cont);
+    if (dynamic_cast<Plusk1*>(k) and v_is_here) {
+      print_indented(indent+1, "plusk1");
+      exp = dynamic_cast<Plusk1*>(k)->exp;
+      k = new Plusk2(v, dynamic_cast<Plusk1*>(k)->k);
+      v_is_here = false;
+      continue;
+    }
 
-      *v = plusk2->v + *v;
-      cont = plusk2->cont;
-    } else if (dynamic_cast<MinusK1*>(cont) and v) {
-      print_indented(indent, "run*minusk1");
-      MinusK1* minusk1 = dynamic_cast<MinusK1*>(cont);
+    if (dynamic_cast<Plusk2*>(k) and v_is_here) {
+      print_indented(indent+1, "plusk2");
+      v = dynamic_cast<Plusk2*>(k)->v + v;
+      k = dynamic_cast<Plusk2*>(k)->k;
+      continue;
+    }
 
-      exp = minusk1->exp;
-      cont = new MinusK2(*v, minusk1->cont);
-      v = NULL;
-    } else if (dynamic_cast<MinusK2*>(cont) and v) {
-      print_indented(indent, "run*minusk2");
-      MinusK2* minusk2 = dynamic_cast<MinusK2*>(cont);
+    if (dynamic_cast<Minusk1*>(k) and v_is_here) {
+      print_indented(indent+1, "minusk1");
+      exp = dynamic_cast<Minusk1*>(k)->exp;
+      k = new Minusk2(v, dynamic_cast<Minusk1*>(k)->k);
+      v_is_here = false;
+      continue;
+    }
 
-      *v = minusk2->v - *v;
-      cont = minusk2->cont;
-    } else if (dynamic_cast<TimesK1*>(cont) and v) {
-      print_indented(indent, "run*timesk1");
-      TimesK1* timesk1 = dynamic_cast<TimesK1*>(cont);
+    if (dynamic_cast<Minusk2*>(k) and v_is_here) {
+      print_indented(indent+1, "minusk2");
+      v = dynamic_cast<Minusk2*>(k)->v - v;
+      k = dynamic_cast<Minusk2*>(k)->k;
+      continue;
+    }
 
-      exp = timesk1->exp;
-      cont = new TimesK2(*v, timesk1->cont);
-      v = NULL;
-    } else if (dynamic_cast<TimesK2*>(cont) and v) {
-      print_indented(indent, "run*timesk2");
-      TimesK2* timesk2 = dynamic_cast<TimesK2*>(cont);
+    if (dynamic_cast<Timesk1*>(k) and v_is_here) {
+      print_indented(indent+1, "timesk1");
+      exp = dynamic_cast<Timesk1*>(k)->exp;
+      k = new Timesk2(v, dynamic_cast<Timesk1*>(k)->k);
+      v_is_here = false;
+      continue;
+    }
 
-      *v = timesk2->v * (*v);
-      cont = timesk2->cont;
-    } else if (dynamic_cast<DivideK1*>(cont) and v) {
-      print_indented(indent, "run*dividek1");
-      DivideK1* dividek1 = dynamic_cast<DivideK1*>(cont);
+    if (dynamic_cast<Timesk2*>(k) and v_is_here) {
+      print_indented(indent+1, "timesk2");
+      v = dynamic_cast<Timesk2*>(k)->v * v;
+      k = dynamic_cast<Timesk2*>(k)->k;
+      continue;
+    }
 
-      exp = dividek1->exp;
-      cont = new DivideK2(*v, dividek1->cont);
-      v = NULL;
-    } else if (dynamic_cast<DivideK2*>(cont) and v) {
-      print_indented(indent, "run*dividek2");
-      DivideK2* dividek2 = dynamic_cast<DivideK2*>(cont);
+    if (dynamic_cast<Dividek1*>(k) and v_is_here) {
+      print_indented(indent+1, "dividek1");
+      exp = dynamic_cast<Dividek1*>(k)->exp;
+      k = new Dividek2(v, dynamic_cast<Dividek1*>(k)->k);
+      v_is_here = false;
+      continue;
+    }
 
-      *v = dividek2->v / *v;
-      cont = dividek2->cont;
-    } else if (dynamic_cast<Halt*>(cont) and v) {
-      print_indented(indent, "run*halt");
+    if (dynamic_cast<Dividek2*>(k) and v_is_here) {
+      print_indented(indent+1, "dividek2");
+      v = dynamic_cast<Dividek2*>(k)->v / v;
+      k = dynamic_cast<Dividek2*>(k)->k;
+      continue;
+    }
 
-      return *v;
-    } else {
-      assert(false);
+    if (dynamic_cast<Halt*>(k) and v_is_here) {
+      print_indented(indent+1, "halt");
+      return v;
     }
   }
 }
-      
+
 int main(int argc, char* argv[]) {
-  assert(run(new Plus(new Numeral(2), new Numeral(2)), 0) == 4);
-  assert(run(new Times(new Numeral(3), new Numeral(3)), 0) == 9);
-  assert(run(new Plus(new Negate(new Numeral(2)),
-		      new Numeral(2)), 0) == 0);
-  assert(run(new Times(new Negate(new Numeral(3)),
-		       new Negate(new Numeral(3))), 0) == 9);
-  assert(run(new Divide(new Negate(new Numeral(9)),
-			new Negate(new Numeral(3))), 0) == 3);
+  printf("test 1\n");
+  assert(calc(new Plus(new Numeral(2), new Numeral(2)), 0) == 4);
+  printf("test 2\n");
+  assert(calc(new Times(new Numeral(3), new Numeral(3)), 0) == 9);
+  printf("test 3\n");
+  assert(calc(new Plus(new Negate(new Numeral(2)),
+		       new Numeral(2)), 0) == 0);
+  printf("test 4\n");
+  assert(calc(new Times(new Negate(new Numeral(3)),
+			new Negate(new Numeral(3))), 0) == 9);
+  printf("test 5\n");
+  assert(calc(new Divide(new Negate(new Numeral(3)),
+			 new Negate(new Numeral(3))), 0) == 1);
   return 0;
 }
